298e822326d22456ec4c3105993dfc179bf6271e
aditya1904 <adityamalu1@gmail.com>
2016-09-09 12:58:11 +0530

    Update amazon_scrap.py

--- amazon_scrap.py
+++ amazon_scrap.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
+##This one needs a lot of improvment 
+## A cleaner version update soon.
 import urllib2
 import lxml.html
 import sys
 
-url = ''
+url = 'http://www.amazon.in/Sony-MDR-ZX110A-Stereo-Headphone-White/dp/B00KGZZ824/ref=sr_1_1?ie=UTF8&qid=1470725118&sr=8-1&keywords=sony+mdr+zx110a'
 
 try:
 	html = urllib2.urlopen(url).read()
@@ -15,6 +16,18 @@ except urllib2.URLError as e:
 
 tree = lxml.html.fromstring(html)
 price_class =  tree.cssselect('td.a-span12 > span#priceblock_saleprice')[0]
+name_class = tree.cssselect('h1#title > span#productTitle')[0]
+name = name_class.text_content()
+try:
+    price_class_deal =  tree.cssselect('td.a-span12 > span#priceblock_dealprice')[0]
+    deal = price_class.text_content()
+    print "dealprice" + deal
+except:
+    pass
+
 price = price_class.text_content()
-print price
+name = name.replace('\n','')
+
+print name
+print "Saleprice" + price
 
