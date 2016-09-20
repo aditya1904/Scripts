9581e9b06a7e8935124a6f9a1835f8762df21ea4
aditya1904 <adityamalu1@gmail.com>
2016-09-20 19:06:37 +0530

    New one added.

--- new.py
+++ new.py
@@ -2,19 +2,30 @@
 
 import smtplib
 import getpass
-
-email = "anonbridge1@gmail.com"
-password = getpass.getpass()
-
-recipient = raw_input('To: ')
-subject = raw_input('Subject: ')
-Body = raw_input('Body: ')
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText  # Added
 from email.mime.image import MIMEImage
+import urllib
+import lxml.html
 
-attachment = 'bob.jpg'
+email = "adityamalu1@gmail.com"
+password = getpass.getpass()
+
+recipient = 'maluaditya12@gmail.com'
+subject = 'Hi.'
+Body = 'Todays xkcd.'
+attachment = 'bob.png'
+
+url = 'http://xkcd.com'
+king = urllib.urlopen(url).read()
+tree =  lxml.html.fromstring(king)
+image_url = tree.cssselect('img')[1].get('src')
+image_url = 'http:' + image_url
+image = urllib.urlopen(image_url).read()
+fd = open('bob.png','w')
+fd.write(image)
+fd.close()
+
 
 msg = MIMEMultipart()
 msg["To"] = recipient
@@ -30,12 +41,11 @@ fp.close()
 img.add_header('Content-ID', '<{}>'.format(attachment))
 msg.attach(img)
 
+print msg.as_string()
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
 server.login(email, password)
 
-BODY = '\r\n'.join(['Subject: %s' % subject, '', Body])
-
 server.sendmail(email, recipient, msg.as_string())
-print ('email sent')
+
